126c61d95db049dde8f443da7920dcc7030d0f5b
Rafael Gioffi <rafaelgioffi@gmail.com>
2023-01-27 14:18:52 -0300

    Foreign key not null (referential integrity)

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -17,6 +17,7 @@ namespace SalesWebMvc.Models
         [DataType(DataType.Currency)]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller() { }
