e5b41ddce39884ec916d50c808938a0f5b6ab7f6
Rafael Gioffi <rafaelgioffi@gmail.com>
2023-01-27 19:20:01 -0300

    Update seller and custom service exception

---
+++ Services/Exception/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Exception
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) :base(message) { }
+    }
+}
